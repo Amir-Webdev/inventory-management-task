6c68dd6934627bf5d68f9fd4ec98dd4fb50168ce
Amir-Webdev <fullstack.dev.amir@gmail.com>
2025-10-15 21:36:05 +0330

    Implemented Transfer API Functions for Fetching and Creating Transfers

---
+++ src/lib/transfers.ts
@@ -0,0 +1,19 @@
+import {
+  Transfer,
+  TransferFormInput,
+  transferSchema,
+  transfersSchema,
+} from "../types";
+import { api, parseOrThrow } from "./api";
+
+export async function getTransfers(): Promise<Transfer[]> {
+  const { data } = await api.get("/api/transfers");
+  return parseOrThrow(transfersSchema, data);
+}
+
+export async function createTransfer(
+  payload: TransferFormInput
+): Promise<Transfer> {
+  const { data } = await api.post("/api/transfers", payload);
+  return parseOrThrow(transferSchema, data);
+}
