5fa05b4872d7348dab989f55a212ea35d4f93d59
Amir-Webdev <fullstack.dev.amir@gmail.com>
2025-10-17 12:23:49 +0330

    Applied New Theme to App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,10 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 import AppLayout from "./_appLayout";
+import { theme } from "../lib/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(
@@ -17,9 +20,12 @@ export default function App({ Component, pageProps }: AppProps) {
   );
   return (
     <QueryClientProvider client={queryClient}>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
